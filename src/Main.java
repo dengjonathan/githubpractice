6e89de9ccb924b7d68fdf0b1c113c0273a192e17
Jonathan Deng <jonathandeng@Jonathans-MacBook-Pro.local>
2019-04-14 17:43:42 -0700

    update string

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Los Angeles! EDITED!!");
+        System.out.println("Hello Los Angeles!- the best city on the West Coast! EDITED!!");
     }
 }
